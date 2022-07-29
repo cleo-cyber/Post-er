3422baae25fa7cd6c649127084b8db217ff16717
cleo-cyber <kadimacleo92@gmail.com>
2022-07-29 17:28:04 +0300

    refactor views

--- tutorial/posts/urls.py
+++ tutorial/posts/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from posts import views
-
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns =[
     path('posts/',views.post_list),
     path('posts/<int:pk>/',views.post_detail),
 ]
+
+
+urlpatterns =format_suffix_patterns(urlpatterns)
