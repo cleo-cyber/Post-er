4902f034663fdb008d440ce0b1e8e4e4aa5b8104
cleo-cyber <kadimacleo92@gmail.com>
2022-08-01 03:46:53 +0300

    generic class based API

--- tutorial/posts/views.py
+++ tutorial/posts/views.py
@@ -11,30 +11,17 @@ from rest_framework import generics
 
 # Get all Posts 
 # Create all posts 
-class PostList(generics.ListModelMixin,mixins.CreateModelMixin,mixins.GenericAPIView):
+class PostList(generics.ListCreateAPIView):
     queryset=Post.objects.all()
-    serializerclass=PostSerializer
+    serializer_class=PostSerializer
 
-    def get(self,request,*args,**kwargs):
-        return self.list(request,*args,**kwargs)
-
-    def post(self,request,*args,**kwargs):
-        return self.create(request,*args,**kwargs)  
 
 
   
 
 
-class PostDetail(generics.GenericAPIView,mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin):
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset=Post.objects.all()
-    serializerclass=PostSerializer
+    serializer_class=PostSerializer
 
-    def get(self,request,*args,**kwargs):
-        return self.retrieve(request,*args,**kwargs)
-
-    def put(self,request,*args,**kwargs):
-        return self.update(request,*args,**kwargs)
-
-    def delete(self,request,*args,**kwargs):
-        return self.destroy(request,*args,**kwargs)
 
